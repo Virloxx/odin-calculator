5a730f3e94b141b54f6845c7c971a6c21a42ef87
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-06-20 14:29:56 +0200

    add display functions
    add UpdateDisplay(), clearDisplay() functions

--- src/script.js
+++ src/script.js
@@ -1,7 +1,43 @@
-var firstNumber = null;
+let firstNumber = null;
 let secondNumber = null;
 let operator = null;
 let displayValue = 0;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+       if(button.classList == "operand"){
+        if (displayValue === 0 || displayValue === '0') {
+            displayValue = button.value;
+        } else {
+            displayValue += button.value;
+        }
+       } 
+        updateDisplay();
+        if(button.value === "clear") {
+            clearDisplay();
+            updateDisplay();
+        }
+    });
+});
+
+function updateDisplay() {
+    const display = document.getElementById("display");
+    display.innerText = displayValue;
+    if (displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+
+function clearDisplay() {
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    displayValue = 0;
+}
+
+const display = document.getElementById("display");
+display.innerText = displayValue;
 
 function operate(op, num1, num2) {
     switch(op) {
@@ -20,8 +56,8 @@ function operate(op, num1, num2) {
             }
             else {
                 return num1 / num2;
-                break;
             }
+            break;
     }
 }
 
